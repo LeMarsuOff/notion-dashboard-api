c04a9ecc48819f1df9d0868f4c2ea821aaec8d9d
LeMarsuOff <maxime-crypto@hotmail.com>
2026-03-18 02:49:38 +0100

    Update trades.js

--- api/trades.js
+++ api/trades.js
@@ -1,36 +1,29 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2759
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+import { Client } from "@notionhq/client";
 
-\f0\fs24 \cf0 import \{ Client \} from "@notionhq/client";\
-\
-const notion = new Client(\{\
-  auth: process.env.NOTION_TOKEN\
-\});\
-\
-export default async function handler(req, res) \{\
-  try \{\
-    const response = await notion.databases.query(\{\
-      database_id: process.env.NOTION_DATABASE_ID\
-    \});\
-\
-    const trades = response.results.map(page => \{\
-      const p = page.properties;\
-\
-      return \{\
-        date: p["Date"]?.date?.start,\
-        pair: p["Pair"]?.title?.[0]?.plain_text,\
-        resultR: p["ResultR"]?.number\
-      \};\
-    \});\
-\
-    res.setHeader("Access-Control-Allow-Origin", "*");\
-    res.status(200).json(trades);\
-\
-  \} catch (error) \{\
-    res.status(500).json(\{ error: error.message \});\
-  \}\
-\}}
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN
+});
+
+export default async function handler(req, res) {
+  try {
+    const response = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID
+    });
+
+    const trades = response.results.map(page => {
+      const p = page.properties;
+
+      return {
+        date: p["Date"]?.date?.start,
+        pair: p["Pair"]?.title?.[0]?.plain_text,
+        resultR: p["ResultR"]?.number
+      };
+    });
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(trades);
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
